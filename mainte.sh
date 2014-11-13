56dad6d8a74ec2f8981c11af93da1ccaf5dccd85
Songmu <y.songmu@gmail.com>
2014-11-13 11:34:43 +0900

    udpate mainte.sh

--- mainte.sh
+++ mainte.sh
@@ -2,4 +2,3 @@
 curl -L https://raw.githubusercontent.com/georgenano/vimrc/master/.vim/compiler/scalac.vim > vimfiles/compiler/scalac.vim
 curl -L https://raw.githubusercontent.com/tommorris/scala-vim-snippets/master/scala.snippets > vimfiles/snippets/scala.snip
 curl -L https://raw.githubusercontent.com/yuroyoro/vim-scala/master/dict/scala.dict > vimfiles/dict/scala.dict
-curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash > .zsh/git-completion.bash
