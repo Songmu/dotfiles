0e9b564073997110acce8e0954e621a2d8cecf04
Songmu <y.songmu@gmail.com>
2016-12-27 17:26:12 +0900

    introduce vim-plug

--- mainte.sh
+++ mainte.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-curl -L https://raw.githubusercontent.com/georgenano/vimrc/master/.vim/compiler/scalac.vim > vimfiles/compiler/scalac.vim
-curl -L https://raw.githubusercontent.com/tommorris/scala-vim-snippets/master/scala.snippets > vimfiles/snippets/scala.snip
-curl -L https://raw.githubusercontent.com/yuroyoro/vim-scala/master/dict/scala.dict > vimfiles/dict/scala.dict
+curl -sL https://raw.githubusercontent.com/georgenano/vimrc/master/.vim/compiler/scalac.vim > vimfiles/compiler/scalac.vim
+curl -sL https://raw.githubusercontent.com/tommorris/scala-vim-snippets/master/scala.snippets > vimfiles/snippets/scala.snip
+curl -sL https://raw.githubusercontent.com/yuroyoro/vim-scala/master/dict/scala.dict > vimfiles/dict/scala.dict
+curl -sL https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > vimfiles/autoload/plug.vim
