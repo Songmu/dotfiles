d04cad49c0ca9939046d410e143f513c9ee9c00c
Songmu <y.songmu@gmail.com>
2024-09-18 20:34:20 +0900

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,27 @@
 local wezterm = require 'wezterm';
 return {
-    font = wezterm.font("Menlo", {weight="Regular", stretch="Normal", style="Normal"}),
-    font_size = 15.0,
+    font = wezterm.font_with_fallback {
+        {
+            family = "HackGen35 Console NF",
+            weight = "Regular",
+            stretch = "Normal",
+            style = "Normal",
+        },
+        {
+            family = "Menlo",
+            weight = "Regular",
+            stretch = "Normal",
+            style = "Normal",
+        }
+    },
+    font_size = 16.0,
     hide_tab_bar_if_only_one_tab = true,
     disable_default_key_bindings = true,
+    keys = {
+        {
+            key = "v",
+            mods = "CMD",
+            action = wezterm.action.PasteFrom "Clipboard",
+        },
+    }
 }
