d7b8c3a5a313b452206f58bb75bd33728f644f22
Songmu <y.songmu@gmail.com>
2024-09-18 19:58:09 +0900

    update

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,4 +2,6 @@ local wezterm = require 'wezterm';
 return {
     font = wezterm.font("Menlo", {weight="Regular", stretch="Normal", style="Normal"}),
     font_size = 15.0,
+    hide_tab_bar_if_only_one_tab = true,
+    disable_default_key_bindings = true,
 }
