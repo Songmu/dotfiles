0218e202eb7375ffe862bc3003fb9b613281c1d2
Songmu <y.songmu@gmail.com>
2025-08-21 23:27:39 +0900

    east asian ambigous with

--- .wezterm.lua
+++ .wezterm.lua
@@ -18,6 +18,7 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     disable_default_key_bindings = true,
     adjust_window_size_when_changing_font_size = false,
+    treat_east_asian_ambiguous_width_as_wide = true,
     keys = {
         {
             key = "v",
